3e89fb72527e84bc15742400ca6e3460c652ca75
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-02 02:44:58 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,13 @@
 
-clean_data <- raw_data
+#Part A: Load and condition the data  
+
+#1.	The data is available on blackboard (hotelSurveySherison), as a JSON file.
+
+ library(jsonlite)                                                          # json lite library is loaded to perform json operations.
+data <- data.frame(fromJSON("hotel_survey.json"))                           # the data from json file is loaded using fromJSON function and converted to data frame using data.frame fucntion. 
+
+
+#2.	Use the str command to make sure you can see the following attributes
+
+str(data)                                                                   # str - represents structure whcih gives us the structure of the data including data types of the column variables in the data frame.
+
