f291a2ddc6c135686023d8051f0440b8453b382c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-05 12:28:55 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,8 +3,8 @@
 
 #1.	The data is available on blackboard (hotelSurveySherison), as a JSON file.
 
- library(jsonlite)                                                          # json lite library is loaded to perform json operations.
-data <- data.frame(fromJSON("hotel_survey.json"))                           # the data from json file is loaded using fromJSON function and converted to data frame using data.frame fucntion. 
+library(jsonlite)                                                          # json lite library is loaded to perform json operations.
+data <- data.frame(fromJSON("hotelSurveySherison.json"))                           # the data from json file is loaded using fromJSON function and converted to data frame using data.frame fucntion. 
 
 
 #2.	Use the str command to make sure you can see the following attributes
